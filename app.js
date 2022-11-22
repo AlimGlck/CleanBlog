8aa3079fcdda6f8befddf39fc9f98b8209acb42b
Alim Gölcük <alimglck5421@gmail.com>
2022-11-22 13:39:17 +0300

    routed

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require("express");
-
-const blog = { id: 1, title: "Blog title", description: "Blog description" };
+const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(blog);
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
