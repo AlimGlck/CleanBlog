d082bd73844e4effe144c9131b607c47e8b20367
Alim Gölcük <alimglck5421@gmail.com>
2022-11-23 16:16:21 +0300

    database added

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+
+const path = require('path');
+
+const Post = require('./models/Post');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
+
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index',{
+    posts,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -23,7 +35,12 @@ app.get('/post', (req, res) => {
   res.render('post');
 });
 
-const port = 3000;
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
+
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server ${port} portunda başlatıldı`);
 });
